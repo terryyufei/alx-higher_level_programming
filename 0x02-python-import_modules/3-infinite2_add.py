9ea1e43d1aab93cb7896690ee7b659edd3058d5c
Terry Wambui <shikuwambui@gmail.com>
2023-05-11 20:53:00 +0300

    infinite numbers

--- 0x02-python-import_modules/3-infinite2_add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from sys import argv
-
-sum = 0
-for arg in argv[1:]:
-    sum += int(arg)
-
-print(sum)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    suma = 0
-    i = 0
-    for a in sys.argv:
-        if i > 0:
-            suma += int(a)
-        i += 1
-    print(suma)
+if __name__ == '__main__':
+    from sys import argv
+
+    sum = 0
+    for arg in argv[1:]:
+        sum += int(arg)
+
+    print(sum)
