955acb6564d7e2a5e51b30bc82bf967ad88cf18b
Terry Wambui <shikuwambui@gmail.com>
2023-05-11 15:51:42 +0300

    import add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
