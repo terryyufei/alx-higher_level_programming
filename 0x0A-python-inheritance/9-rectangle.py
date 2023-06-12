797bdf94404a8f50d23c297a9256ccadbc1e0767
Terry Wambui <shikuwambui@gmail.com>
2023-06-12 21:16:13 +0300

    full rectangle, area + str()

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""The module is 8-base_geometry"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A subclass of BaseGeometry class"""
+
+    def __init__(self, width, height):
+        """initialize private attributes"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """returns area of a rectangle and overrides area() from superclass"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a human readable string representation"""
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
