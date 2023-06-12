48ae0c9b347a56e54f8a82e937492b94f7ba0e7c
Terry Wambui <shikuwambui@gmail.com>
2023-06-12 19:32:10 +0300

    improve geometry by raising an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""The module is 6-base_geometry"""
+
+
+class BaseGeometry:
+    """Defines BaseGeometry class"""
+
+    def area(self):
+        """Raise an exception"""
+        raise Exception("area() is not implemented")
