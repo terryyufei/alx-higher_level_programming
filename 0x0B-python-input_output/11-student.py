d503662c172c07a46eb9d0ccbb83e43ee7fce7ef
Terry Wambui <shikuwambui@gmail.com>
2023-06-13 21:15:05 +0300

    a public method that replaces all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Write a class"""
+
+
+class Student:
+    """Defines a class Student"""
+    def __init__(self, first_name, last_name, age):
+        """initializes the attributes of class Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+
+        else:
+            d = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    d[attr] = getattr(self, attr)
+            return d
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for keys in json:
+            setattr(self, keys, json[keys])
