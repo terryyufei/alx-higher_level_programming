ad453d429a04b07ad27b692f1c2fa33991e4a73f
Terry Wambui <shikuwambui@gmail.com>
2023-06-13 17:50:42 +0300

    a function that reads text from a file and prints to the tsdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Read from a file"""
+
+
+def read_file(filename=""):
+    """Reads text from a file and prints to stdout"""
+    with open(filename) as file:
+        content = file.read()
+    print(content)
