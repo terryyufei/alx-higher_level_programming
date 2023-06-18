d7d1adf12a6385a43dfd76aa256838859f0014f4
Terry Wambui <shikuwambui@gmail.com>
2023-06-18 16:39:22 +0300

    JSON ok, but CSV?

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 """Write the first class Base"""
 
 import json
+import csv
 
 
 class Base:
@@ -43,3 +44,67 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return (dummy)
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        try:
+            with open(cls.__name__ + ".json", "r") as file:
+                list = cls.from_json_string(file.read())
+                return [cls.create(**dic) for dic in list]
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """saves a list of objects to a CSV file"""
+        filename = cls.__name__ + ".csv"  # creates a filename for the CSV file
+        with open(filename, "w", newline="") as file:
+            if list_objs is None or list_objs == []:
+                file.write("[]")  # Return an empty list
+            else:
+                if cls.__name__ == "Rectangle":
+                    # assign fieldnames depending on the object
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+
+                # write dictionaries to the CSV file
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                for obj in list_objs:
+                    # converts the object to a dictionary
+                    writer.writerow(obj.to_dictionary)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """reads data from a CSV file and
+        returns a list of instantiated classes
+        """
+        filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as file:
+                if cls.__name__ == "Rectangle":
+                    # Depending on the class name, different fieldnames
+                    #  will be used for reading data from the CSV file
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                # reads data from the CSV file
+                list = csv.DictReader(file, fieldnames=fieldnames)
+
+                # convert the read dictionaries into a list of dictionaries
+                list = [dict([k, int(v)] for k, v in d.items())
+                        for d in list]
+                return [cls.create(**d) for d in list]
+        except IOError:
+            return []
