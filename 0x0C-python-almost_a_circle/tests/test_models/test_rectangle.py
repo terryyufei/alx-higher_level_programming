41520ed3d05dd0c4832d02c6c7bd4cbe7ca0e919
Terry Wambui <shikuwambui@gmail.com>
2023-06-19 22:53:36 +0300

    unit testing

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# test_rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
+
 """Defines unittests for models/rectangle.py.
 Unittest classes:
     TestRectangle_instantiation - line 25
