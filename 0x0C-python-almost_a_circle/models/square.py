2deb20fb56122bed21b47adac5bd914866b8dbf2
Terry Wambui <shikuwambui@gmail.com>
2023-06-19 19:16:27 +0300

    And now, the Square!

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """class constructor"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """Returns a string representation of a square instance"""
