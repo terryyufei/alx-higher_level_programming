08a0db2325b13ceb0a4d1536f25a0cd72c43958b
Terry Wambui <shikuwambui@gmail.com>
2023-07-18 16:32:20 +0300

    list by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- lists all records of the table second_table
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+
+SELECT score, name
+FROM second_table ORDER BY score DESC; 
