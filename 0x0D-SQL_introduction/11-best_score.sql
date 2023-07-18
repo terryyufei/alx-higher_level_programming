5e1415b3ffcce25d8f48e442d8cdfb80fb4eec48
Terry Wambui <shikuwambui@gmail.com>
2023-07-18 16:37:39 +0300

    select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- lists all records with a score >= 10 in sceond_table
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+
+SELECT score, NAME
+FROM second_table
+WHERE score >= 10 ORDER BY score DESC;
