07f1f215a60f479f72b34d408676352083c35d4a
Terry Wambui <shikuwambui@gmail.com>
2023-07-18 16:38:39 +0300

    select the best

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -2,6 +2,6 @@
 -- Results should display both the score and the name (in this order)
 -- Records should be ordered by score (top first)
 
-SELECT score, NAME
+SELECT score, name
 FROM second_table
 WHERE score >= 10 ORDER BY score DESC;
