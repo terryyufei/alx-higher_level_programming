7a62c1db68497c8932bc4e2b562dac2b5c4e9ce1
Terry Wambui <shikuwambui@gmail.com>
2023-07-18 15:18:00 +0300

    describe my table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- prints the full description of the table first_table from hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+
+SHOW CREATE TABLE first_table;
