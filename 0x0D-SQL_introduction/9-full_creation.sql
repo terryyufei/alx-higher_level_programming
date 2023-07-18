e7f92710bd34bf26c1b3bbee9ad0a8e0a5649bfc
Terry Wambui <shikuwambui@gmail.com>
2023-07-18 16:18:22 +0300

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,19 @@
+-- creates a table second_table in hbtn_0c_0
+-- attributes are id, name, score
+-- If the table second_table already exists, your script should not fail
+-- You are not allowed to use the SELECT and SHOW statements
+
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES(1, "John", 10);
+INSERT INTO second_table (id, name, score)
+VALUES(2, "Alex", 3);
+INSERT INTO second_table (id, name, score)
+VALUES(3, "Bob", 14);
+INSERT INTO second_table (id, name, score)
+VALUES( 4, "George", 8);
