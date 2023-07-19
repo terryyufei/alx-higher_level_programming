8f6d0d3049d0c1fb9c08fcf4cc73d5444e02d149
Terry Wambui <shikuwambui@gmail.com>
2023-07-19 15:35:58 +0300

    check your privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- show privilege from user 1 and user 2
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR `user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_'2@'localhost';
