78f26bb77579e1f3ca2a0a17be476828c96c2f2a
Terry Wambui <shikuwambui@gmail.com>
2023-07-19 13:40:16 +0300

    check your privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- lists all the privilegs of MySQL users user_0d_1 and user_0d_2
-
+-- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
