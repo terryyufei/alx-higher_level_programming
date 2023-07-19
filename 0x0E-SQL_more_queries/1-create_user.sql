5c160464ddd2e7ae3ccf6ecef1467cf1c00a39db
Terry Wambui <shikuwambui@gmail.com>
2023-07-19 15:18:36 +0300

    I am Root

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- creates the MySQL server user user_0d_1
-
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost`IDENTIFIED BY `user_0d_1_pwd`;
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+-- create a new database and a new user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2' @'localhost';
