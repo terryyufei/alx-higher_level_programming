68e81c763aa079b7f00a43134af31f320ece413b
Terry Wambui <shikuwambui@gmail.com>
2023-07-19 22:08:48 +0300

    only comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,13 @@
+-- lists all comedy shows in hbtn_0d_tvshows
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use only one SELECT statement
+
+--  a script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "comedy"
+ORDER BY tv_shows.title;
