149be14acfe66ee53a198ff6013b224846803b7a
Terry Wambui <shikuwambui@gmail.com>
2023-08-15 01:46:10 +0300

    Increment Object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,11 +4,7 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-
 myObject.incr = () => { myObject.value += 1; };
-myObject.incr();
-console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
