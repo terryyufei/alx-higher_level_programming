2a7cabda76d10f5adbc2cfff3e28db9148ef31ee
Terry Wambui <shikuwambui@gmail.com>
2023-08-15 23:42:39 +0300

    an instance that prints a square

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+};
