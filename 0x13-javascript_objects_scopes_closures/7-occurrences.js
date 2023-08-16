89ae9503eac452a6fd8a113def318869b8aef409
Terry Wambui <shikuwambui@gmail.com>
2023-08-16 03:49:18 +0300

    number of occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let occur = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      occur++;
+    }
+  }
+  return occur;
+};
