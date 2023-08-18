4b369a62eeffa5e2bbe84ca8f5d949e753ff3d9d
Terry Wambui <shikuwambui@gmail.com>
2023-08-18 15:19:43 +0300

    a script that connects and fetches all states from a db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,16 +8,21 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
+    # Connect database using command-line arguments
     my_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
                             db=argv[3], port=3306)
+    # Create cursor obj to interact with database
     my_cursor = my_db.cursor()
 
+    # Execute a SELECT query to fetch data
     my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
 
+    # fetch all the data returned by the query
     my_data = my_cursor.fetchall()
 
-    for data in my_data:
-        print(data)
+    # Iterate through the fetched data and print each row
+    for row in my_data:
+        print(row)
 
     # Close all cursors
     my_cursor.close()
