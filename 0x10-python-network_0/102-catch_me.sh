56fb244deb8f6415801bcebef2df2a478b2d5c02
Terry Wambui <shikuwambui@gmail.com>
2023-08-31 22:44:53 +0300

    catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that get the message You got me!
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL 0:5000/catch_me -X PUT "You got me!" -H "Origin: X-School-User-Id" -d "user_id=98"
