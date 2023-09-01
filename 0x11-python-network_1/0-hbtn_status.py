7a0fb819c9e45b299fc1fd8d83c1c40755828fa4
Terry Wambui <shikuwambui@gmail.com>
2023-09-01 21:03:14 +0300

    fetching using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
 
         # Display the response body with tabulation
         print("Body response:")
-        print("\t - type: {}".format(type(response_body)))
-        print("\t - content: {}".format(response_body))
-        print("\t - utf8 content: {}".format(response_body.decode('utf-8')))
+        print("\t- type: {}".format(type(response_body)))
+        print("\t- content: {}".format(response_body))
+        print("\t- utf8 content: {}".format(response_body.decode('utf-8')))
