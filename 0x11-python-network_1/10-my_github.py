47f8f8a9f3c5f8f03230b7afa7688ed1c0963fd8
Terry Wambui <shikuwambui@gmail.com>
2023-09-02 03:39:00 +0300

    github api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,6 +3,7 @@
 
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
@@ -13,9 +14,9 @@ if __name__ == "__main__":
     # GitHub API URL
     url = 'https://api.github.com/user'
 
-    # headers for basic authentication
-    headers = {'Authorization': f'Basic {username} {password}'}
+    # authentication
+    auth = HTTPBasicAuth(username, password)
 
     # send a GET request to the GitHub API
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, auth=auth)
     print(response.json().get("id"))
