015dafdc4416e5120453e4692cb361013caac7d1
Terry Wambui <shikuwambui@gmail.com>
2023-09-26 20:08:12 +0300

    request and write

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+// import the module
+const request = require('request');
+const fs = require('fs');
+
+// The first argument is the URL to request
+const apiUrl = process.argv[2];
+
+// The second argument the file path to store the body response
+const file = process.argv[3];
+
+// Make an HTTP GET request to the specified URL
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    // write response to file
+    fs.writeFile(file, body, 'utf-8', (error) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      console.log(file);
+    });
+  }
+});
