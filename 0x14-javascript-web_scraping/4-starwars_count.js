8bd34e66ae0e19895d0b86903104ce82421c1632
Terry Wambui <shikuwambui@gmail.com>
2023-09-26 19:30:13 +0300

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+// import the module
+const request = require('request');
+
+// The first argument is the API URL
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+
+// Make an HTTP GET request to the API
+request(apiUrl, function (error, response, body) {
+  if (!error) {
+    // parse the JSON response
+    const results = JSON.parse(body).results;
+
+    // count no of movies where "Wedge Antilles" is present
+    const moviesWithWedge = results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1 // if found
+        : count; // if not found
+    }, 0); // initiliazing count to zero
+    console.log(moviesWithWedge);
+  }
+});
