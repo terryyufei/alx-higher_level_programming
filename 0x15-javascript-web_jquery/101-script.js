d2c8baac3369fbc9faaa6ed1978d47b019a08426
Terry Wambui <shikuwambui@gmail.com>
2023-10-04 22:00:34 +0300

    append, remove & empty that list

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,23 @@
+$('document').ready(function () {
+    $('DIV#add_item').click(() => {
+        $('UL.my_list').append('<li>Item</li>');
+    });
+    $('DIV#remove_item').click(() => {
+        $('UL.my_list li:last').remove();
+    });
+    $('DIV#clear_list').click(() => {
+        $('UL.my_list').empty();
+
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
